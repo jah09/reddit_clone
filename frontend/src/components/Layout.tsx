2abc6d2d8c0e92a949ab512f7649702b0ff10aa3
jah09 <jasuhot1@gmail.com>
2024-05-23 22:15:02 +0800

    Working with navbar

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import Navbar from './Navbar'
+import LeftSidenav from "./LeftSidenav";
+import RightSidenav from "./RightSidenav";
+
+
 function Layout({ children }: { children: React.ReactNode }) {
 	return (
-		<div>
-			<Navbar />
-			{children}
-		</div>
-	);
+    <div>
+      <Navbar />
+      <div className=" bg-background flex min-h-screen">
+        <div className="w-[20%] ">
+          <LeftSidenav />
+        </div>
+        <div className="w-[60%]"> {children}</div>
+        <div className="w-[20%]">
+          <RightSidenav />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Layout
---
+++ frontend/src/components/LeftSidenav.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sidenav() {
+  return (
+    <div className="  p-4 h-full text-foreground  border-r border-gray-800">
+      Left sidenav is working
+    </div>
+  );
+}
+
+export default Sidenav

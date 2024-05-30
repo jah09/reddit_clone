3ac498d92407834bc9bebd2dc5f374d0710e4e0b
jah09 <jasuhot1@gmail.com>
2024-05-30 09:11:34 +0800

    add left side nav items

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
 import { Button } from '@/components/ui/Button';
 import Layout from '@/components/Layout';
-import LoginModal from './components/LoginModal';
+import LoginModal from './components/modals/LoginModal';
 import Home from './components/Home';
 
-import SignupModal from './components/SignupModal';
+import SignupModal from './components/modals/SignupModal';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
--- frontend/src/components/LeftSidenav.tsx
+++ frontend/src/components/LeftSidenav.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
-
+import React from "react";
+import add from "../assets/add.svg";
 function Sidenav() {
   return (
     <div className="  p-4 h-full text-foreground  border-r border-gray-800">
-      Left sidenav is working
+      <div className="">
+        <ul>
+          <li className="py-2 hover:bg-[#181c1f] cursor-pointer">Home</li>
+          <li className="py-2 hover:bg-[#181c1f] cursor-pointer">Popular</li>
+          <li className="py-2 hover:bg-[#181c1f] cursor-pointer">All</li>
+        </ul>
+        <hr className="border-gray-700 mt-2" />
+        <div className="mt-6">
+          <p>CUSTOM FEEDS</p>
+        </div>
+        <hr className="border-gray-700 mt-12" />
+        <div className="mt-6">
+          <p>COMMUNITIES</p>
+          {/* <div className="mt-2 flex hover:bg-[#181c1f] hover:rounded-lg">
+        
+            <h6 className=" py-2">Create Community</h6>
+          </div> */}
+          <div className="mt-2 ">
+            <button className="inline-flex items-center hover:bg-[#181c1f] ">
+            <img src={add} alt="add icon ml-10" className="w-8 h-8" />
+              Create Community
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default Sidenav
+export default Sidenav;

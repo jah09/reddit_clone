ce47bd07b93b5c63216bb875cc894df8fa2ecb0e
jah09 <jasuhot1@gmail.com>
2024-06-06 09:00:16 +0800

    layout changed from children to outlet, so that component render dynamically

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,26 @@
-import { Button } from '@/components/ui/Button';
-import Layout from '@/components/Layout';
-import LoginModal from './components/modals/LoginModal';
-import Home from './components/Home';
+import { Button } from "@/components/ui/Button";
+import Layout from "@/components/Layout";
+import LoginModal from "./components/modals/LoginModal";
+import Home from "./components/Home";
 
-import SignupModal from './components/modals/SignupModal';
+import SignupModal from "./components/modals/SignupModal";
 import SubredditForm from "./components/modals/SubredditForm";
-import CreatePost from './components/Home';
+import CreatePost from "./components/CreatePost";
 
-
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-	return (
+  return (
     <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-
-          <Route path="/login" element={<LoginModal />} />
-          <Route path="/signup" element={<SignupModal />} />
-
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<Home />} />
           <Route path="/create-subreddit-form" element={<SubredditForm />} />
           <Route path="/create-post" element={<CreatePost />} />
-
-
-        </Routes>
-      </Layout>
+        </Route>
+        <Route path="/login" element={<LoginModal />} />
+        <Route path="/signup" element={<SignupModal />} />
+      </Routes>
     </BrowserRouter>
   );
 }
